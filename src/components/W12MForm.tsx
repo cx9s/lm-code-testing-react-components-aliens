76cc719617bd3f582860c2376a40e8bd75e3258a
cx9s <cx9918@gmail.com>
2023-02-08 08:27:24 +0000

    add SpeciesName

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,13 +1,19 @@
-import { useState } from 'react';
-import W12MHeader from './W12MHeader';
+import { useState } from "react";
+import W12MHeader from "./W12MHeader";
+import SpeciesName from "./species_name";
 
 const W12MForm = () => {
-	return (
-		<section className='w12MForm'>
-			<W12MHeader />
-			{/* REST OF FORM GOES HERE */}
-		</section>
-	);
+  const [speciesName, setSpeciesName] = useState<string>("humans");
+
+  return (
+    <section className="w12MForm">
+      <W12MHeader />
+      <SpeciesName
+        speciesName={speciesName}
+        onChangeSpeciesName={(e: any) => setSpeciesName(e.target.value)}
+      />
+    </section>
+  );
 };
 
 export default W12MForm;
---
+++ src/components/species_name.tsx
@@ -0,0 +1,21 @@
+interface SpeciesNameProps {
+  speciesName: string;
+  onChangeSpeciesName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SpeciesName: React.FC<SpeciesNameProps> = ({
+  speciesName,
+  onChangeSpeciesName,
+}) => (
+  <div>
+    <label htmlFor="speciesName">Species Name:</label>
+    <input
+      id="speciesName"
+      type="text"
+      value={speciesName}
+      onChange={onChangeSpeciesName}
+    />
+  </div>
+);
+
+export default SpeciesName;
