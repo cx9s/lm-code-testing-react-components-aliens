adb51dc921c1ed945c8464d4d4579e8974fc8ff6
cx9s <cx9918@gmail.com>
2023-02-08 14:18:21 +0000

    add ShowFormInfo

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,45 +1,77 @@
-import { useState } from "react";
+import { ReactEventHandler, useState } from "react";
 import W12MHeader from "./W12MHeader";
 import SpeciesName from "./species_name";
 import PlanetName from "./planet_name";
 import NumberBeings from "./number_beings";
-import Reason from "./reason";
 import Calculator from "./calculator";
+import Reason from "./reason";
+import ShowFormInfo from "./show_form_info";
 
 const W12MForm = () => {
   const [speciesName, setSpeciesName] = useState<string>("humans");
   const [planetName, setPlanetName] = useState<string>("Earth");
   const [numberBeings, setNumberBeings] = useState<number>(0);
-  const [reason, setReason] = useState<string>("reason");
   const [calculator, setCalculator] = useState<boolean>(true);
+  const [reason, setReason] = useState<string>("reason");
+  const [submitted, setSubmitted] = useState<boolean>(false);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
 
   return (
     <section className="w12MForm">
       <W12MHeader />
-      <SpeciesName
-        speciesName={speciesName}
-        onChangeSpeciesName={(e: any) => setSpeciesName(e.target.value)}
-      />
-      <PlanetName
-        planetName={planetName}
-        onChangePlanetName={(e: any) => setPlanetName(e.target.value)}
-      />
-      <NumberBeings
-        numberBeings={numberBeings}
-        onChangeNumberBeings={(e: any) => setNumberBeings(e.target.value)}
-      />
-      <Calculator
-        calculator={calculator}
-        onChangeCalculator={(e: any) =>
-          setCalculator(e.target.value === "true" ? true : false)
-        }
-      />
-      <Reason
-        reason={reason}
-        onChangeReason={(e: any) => {
-          setReason(e.target.value);
-        }}
-      />
+      <form onSubmit={onSubmit}>
+        <SpeciesName
+          speciesName={speciesName}
+          onChangeSpeciesName={(e: any) => {
+            setSubmitted(false);
+            setSpeciesName(e.target.value);
+          }}
+        />
+        <PlanetName
+          planetName={planetName}
+          onChangePlanetName={(e: any) => {
+            setSubmitted(false);
+            setPlanetName(e.target.value);
+          }}
+        />
+        <NumberBeings
+          numberBeings={numberBeings}
+          onChangeNumberBeings={(e: any) => {
+            setSubmitted(false);
+            setNumberBeings(e.target.value);
+          }}
+        />
+        <Calculator
+          calculator={calculator}
+          onChangeCalculator={(e: any) => {
+            setSubmitted(false);
+            setCalculator(e.target.value === "true" ? true : false);
+          }}
+        />
+        <Reason
+          reason={reason}
+          onChangeReason={(e: any) => {
+            setSubmitted(false);
+            setReason(e.target.value);
+          }}
+        />
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+      {submitted && (
+        <ShowFormInfo
+          speciesName={speciesName}
+          planetName={planetName}
+          numberBeings={numberBeings}
+          calculator={calculator}
+          reason={reason}
+        />
+      )}
     </section>
   );
 };
---
+++ src/components/show_form_info.tsx
@@ -0,0 +1,25 @@
+interface ShowFormInfoProps {
+  speciesName: string;
+  planetName: string;
+  numberBeings: number;
+  calculator: boolean;
+  reason: string;
+}
+
+const ShowFormInfo: React.FC<ShowFormInfoProps> = ({
+  speciesName,
+  planetName,
+  numberBeings,
+  calculator,
+  reason,
+}) => (
+  <div>
+    <p>Species Name: {speciesName}</p>
+    <p>Planet Name: {planetName}</p>
+    <p>Number of beings: {numberBeings}</p>
+    <p>What is 2 + 2? {calculator.toString()}</p>
+    <p>Reason for sparing: {reason}</p>
+  </div>
+);
+
+export default ShowFormInfo;
