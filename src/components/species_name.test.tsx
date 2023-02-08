f6b09e8b4cf07f6914ff5a8f2b98f65b55bc8c96
cx9s <cx9918@gmail.com>
2023-02-08 16:11:55 +0000

    refactor species_name.test.tsx

--- src/components/species_name.test.tsx
+++ src/components/species_name.test.tsx
@@ -2,28 +2,28 @@ import { render, screen } from "@testing-library/react";
 import SpeciesName, { SpeciesNameProps } from "./species_name";
 import user from "@testing-library/user-event";
 
-test("renders SpeciesName", () => {
-  render(<SpeciesName speciesName="" onChangeSpeciesName={() => {}} />);
-  const speciesNameText = screen.getByText(/Species Name:/i);
-  expect(speciesNameText).toBeInTheDocument();
-});
+test("Given the required props, When the component is rendered, Then the label and input should be present", () => {
+  const props: SpeciesNameProps = {
+    speciesName: "speciesName",
+    onChangeSpeciesName: jest.fn(),
+  };
 
-test("renders SpeciesName input", () => {
-  render(
-    <SpeciesName speciesName="speciesName" onChangeSpeciesName={() => {}} />
-  );
-  const speciesNameInput = screen.getByDisplayValue("speciesName");
+  render(<SpeciesName {...props} />);
+  const speciesNamelabel = screen.getByText(/Species Name:/i);
+  expect(speciesNamelabel).toBeInTheDocument();
+
+  const speciesNameInput = screen.getByDisplayValue(props.speciesName);
   expect(speciesNameInput).toBeInTheDocument();
   expect(speciesNameInput).toHaveAttribute("id", "speciesName");
 });
 
-test("correctly calls handle change", async () => {
+test("Given the required props, When the value of input changed, Then the function should be present", async () => {
   const props: SpeciesNameProps = {
     speciesName: "speciesName",
     onChangeSpeciesName: jest.fn(),
   };
   render(<SpeciesName {...props} />);
-  const input = screen.getByDisplayValue("speciesName");
+  const input = screen.getByDisplayValue(props.speciesName);
   await user.type(input, "human");
   expect(props.onChangeSpeciesName).toHaveBeenCalledTimes(5);
 });
