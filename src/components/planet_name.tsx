93759d8e60e9f5236b3dcd21dd14b25efb71af0c
cx9s <cx9918@gmail.com>
2023-02-09 16:40:19 +0000

    add validation for planet_name

--- src/components/planet_name.tsx
+++ src/components/planet_name.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import ErrorMessage from "./error_message";
 export interface PlanetNameProps {
   planetName: string;
   onChangePlanetName: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -6,17 +8,37 @@ export interface PlanetNameProps {
 const PlanetName: React.FC<PlanetNameProps> = ({
   planetName,
   onChangePlanetName,
-}) => (
-  <div>
-    <label htmlFor="planetName">Planet Name:</label>
-    <input
-      id="planetName"
-      type="text"
-      placeholder="planet"
-      value={planetName}
-      onChange={onChangePlanetName}
-    />
-  </div>
-);
+}) => {
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const message = validate(e.target.value);
+    setErrorMessage(message);
+    onChangePlanetName(e);
+  };
+
+  const validate: (value: string) => string | undefined = (value) => {
+    const message = /^[a-zA-Z0-9]{2,49}$/.test(value)
+      ? undefined
+      : "Must be between 2 and 49 characters. Numbers are allowed, but no special characters.";
+    return message;
+  };
+
+  return (
+    <div>
+      <label htmlFor="planetName">Planet Name:</label>
+      <input
+        id="planetName"
+        type="text"
+        placeholder="planet"
+        value={planetName}
+        onChange={onChangeHandler}
+      />
+      <ErrorMessage errorMessage={errorMessage} />
+    </div>
+  );
+};
 
 export default PlanetName;
