01cbdc001e63d3e3e16b3c0d46bc3ae48ed70477
cx9s <cx9918@gmail.com>
2023-02-10 22:44:37 +0000

    add test for validation

--- src/components/reason.test.tsx
+++ src/components/reason.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Reason, { ReasonProps } from "./reason";
+import user from "@testing-library/user-event";
 
 const props: ReasonProps = {
   reason: "some reasons",
@@ -10,4 +11,14 @@ test("Given the required props, When the component is rendered, Then the label a
   render(<Reason {...props} />);
   expect(screen.getByText(/Reason for sparing:/i)).toBeInTheDocument();
   expect(screen.getByDisplayValue(props.reason)).toBeInTheDocument();
+});
+
+test("Given the required props, When the value of input is invalid, Then the error message should be present", async () => {
+  render(<Reason {...props} />);
+  const input = screen.getByDisplayValue(props.reason);
+  await user.type(input, "human");
+  const errorMessage = screen.getByText(
+    /Must be between 17 and 153 characters./i
+  );
+  expect(errorMessage).toBeInTheDocument();
 });
