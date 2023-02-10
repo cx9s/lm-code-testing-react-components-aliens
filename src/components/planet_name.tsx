654d897cb6b08ea1bd3d74a863b8c3ee14c8139c
cx9s <cx9918@gmail.com>
2023-02-10 13:50:58 +0000

    pass validated to Form

--- src/components/planet_name.tsx
+++ src/components/planet_name.tsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import ErrorMessage from "./error_message";
 export interface PlanetNameProps {
   planetName: string;
-  onChangePlanetName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangePlanetName: (
+    validated: boolean,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => void;
 }
 
 const PlanetName: React.FC<PlanetNameProps> = ({
@@ -14,16 +17,17 @@ const PlanetName: React.FC<PlanetNameProps> = ({
   );
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const message = validate(e.target.value);
-    setErrorMessage(message);
-    onChangePlanetName(e);
+    const validated = validate(e.target.value);
+    setErrorMessage(
+      validated
+        ? undefined
+        : "Must be between 2 and 49 characters. Numbers are allowed, but no special characters."
+    );
+    onChangePlanetName(validated, e);
   };
 
-  const validate: (value: string) => string | undefined = (value) => {
-    const message = /^[a-zA-Z0-9]{2,49}$/.test(value)
-      ? undefined
-      : "Must be between 2 and 49 characters. Numbers are allowed, but no special characters.";
-    return message;
+  const validate: (value: string) => boolean = (value) => {
+    return /^[a-zA-Z0-9]{2,49}$/.test(value);
   };
 
   return (
