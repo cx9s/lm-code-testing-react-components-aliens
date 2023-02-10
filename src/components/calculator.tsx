ca1210e4bbc064114948490a439ff783cecbdac2
cx9s <cx9918@gmail.com>
2023-02-10 12:24:44 +0000

    add validation for calculator

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import ErrorMessage from "./error_message";
+
 export interface CalculatorProps {
   calculator: boolean;
   onChangeCalculator: (e: React.ChangeEvent<HTMLSelectElement>) => void;
@@ -7,14 +10,30 @@ const Calculator: React.FC<CalculatorProps> = ({
   calculator,
   onChangeCalculator,
 }) => {
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const message = validate(e.target.value);
+    setErrorMessage(message);
+    onChangeCalculator(e);
+  };
+
+  const validate: (value: string) => string | undefined = (value) => {
+    const message = value === "true" ? undefined : '"4" must be selected.';
+    return message;
+  };
+
   return (
     <div>
       <label htmlFor="calculator">What is 2 + 2?</label>
 
-      <select id="calculator" onChange={onChangeCalculator}>
+      <select id="calculator" onChange={onChangeHandler}>
         <option value="true">4</option>
         <option value="false">Not 4</option>
       </select>
+      <ErrorMessage errorMessage={errorMessage} />
     </div>
   );
 };
