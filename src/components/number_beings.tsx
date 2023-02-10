81faa0531dc69e4626cf3ff113e90bb6f1e7ba1a
cx9s <cx9918@gmail.com>
2023-02-10 12:50:30 +0000

    add validation for number_beings

--- src/components/number_beings.tsx
+++ src/components/number_beings.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { isNumber } from "util";
+import ErrorMessage from "./error_message";
+
 export interface NumberBeingsProps {
   numberBeings: number;
   onChangeNumberBeings: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -6,17 +10,38 @@ export interface NumberBeingsProps {
 const NumberBeings: React.FC<NumberBeingsProps> = ({
   numberBeings,
   onChangeNumberBeings,
-}) => (
-  <div>
-    <label htmlFor="numberBeings">Number of beings:</label>
-    <input
-      id="numberBeings"
-      type="text"
-      placeholder="number"
-      value={numberBeings}
-      onChange={onChangeNumberBeings}
-    />
-  </div>
-);
+}) => {
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const message = validate(e.target.value);
+    setErrorMessage(message);
+    onChangeNumberBeings(e);
+  };
+
+  const validate: (value: string) => string | undefined = (value) => {
+    const message =
+      Number.isInteger(+value) && +value >= 10 ** 9
+        ? undefined
+        : "Numbers ONLY. Must be at least 1,000,000,000.";
+    return message;
+  };
+
+  return (
+    <div>
+      <label htmlFor="numberBeings">Number of beings:</label>
+      <input
+        id="numberBeings"
+        type="text"
+        placeholder="number"
+        value={numberBeings}
+        onChange={onChangeHandler}
+      />
+      <ErrorMessage errorMessage={errorMessage} />
+    </div>
+  );
+};
 
 export default NumberBeings;
