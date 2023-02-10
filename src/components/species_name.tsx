b4bce7cfcc7290cb65912c8dc25b8eec770ba28a
cx9s <cx9918@gmail.com>
2023-02-10 13:46:39 +0000

    add validated and pass to Form

--- src/components/species_name.tsx
+++ src/components/species_name.tsx
@@ -3,7 +3,10 @@ import ErrorMessage from "./error_message";
 
 export interface SpeciesNameProps {
   speciesName: string;
-  onChangeSpeciesName: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeSpeciesName: (
+    validated: boolean,
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => void;
 }
 
 const SpeciesName: React.FC<SpeciesNameProps> = ({
@@ -15,16 +18,17 @@ const SpeciesName: React.FC<SpeciesNameProps> = ({
   );
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const message = validate(e.target.value);
-    setErrorMessage(message);
-    onChangeSpeciesName(e);
+    const validated = validate(e.target.value);
+    setErrorMessage(
+      validated
+        ? undefined
+        : "Must be between 3 and 23 characters. No numbers or special characters allowed!"
+    );
+    onChangeSpeciesName(validated, e);
   };
 
-  const validate: (value: string) => string | undefined = (value) => {
-    const message = /^[a-zA-Z]{3,23}$/.test(value)
-      ? undefined
-      : "Must be between 3 and 23 characters. No numbers or special characters allowed!";
-    return message;
+  const validate: (value: string) => boolean = (value) => {
+    return /^[a-zA-Z]{3,23}$/.test(value) ? true : false;
   };
 
   return (
